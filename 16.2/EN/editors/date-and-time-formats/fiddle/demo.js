d848fcd0f7d031b70ae218bac1f1874104374ea8
igniteui-deploy <igniteui@infragistics.com>
2017-02-21 09:30:17 +0000

    Samples for 16.2

--- 16.2/EN/editors/date-and-time-formats/fiddle/demo.js
+++ 16.2/EN/editors/date-and-time-formats/fiddle/demo.js
@@ -1,29 +1,30 @@
 $(function () {
 
     $("#defaultDateEditor").igDateEditor({
-        width: 200,
         value: new Date(2015, 9, 9)
     });
 
     $("#dateTimeEditor").igDateEditor({
-        width: 200,
         dateInputFormat: "dateTime",
         value: new Date(2015, 9, 9, 9, 9)
     });
 
     $("#timeEditor").igDateEditor({
-        width: 200,
         dateInputFormat: "time",
         value: new Date(2015, 9, 9, 9, 9)
     });
 
     $("#constraintEditor").igDateEditor({
-        width: 200,
+        dateInputFormat: "MM/dd/yyyy",
+        dateDisplayFormat: "dddd, MMM d, yyyy",
         minValue: new Date(2012, 9, 24),
-        maxValue: new Date(2018, 9, 24),
-        value: new Date(2015, 9, 9),
-        nullText: "Enter Today's Date",
-        unfilledCharsPrompt: " "
+        maxValue: new Date(2017, 9, 24),
+        value: new Date(2017, 9, 9)
     });
 
+    $("#setMax").igButton({
+        click: function () {
+            $("#constraintEditor").igDateEditor("value", new Date(2018, 9, 26));
+        }
+    });
 });
