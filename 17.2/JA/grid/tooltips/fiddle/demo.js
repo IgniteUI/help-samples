fe8548d5889beaf14dc7ae0f4607f017334c0e36
igniteui-deploy <igniteui@infragistics.com>
2017-11-08 16:19:12 +0000

    Samples for 17.2

--- 17.2/JA/grid/tooltips/fiddle/demo.js
+++ 17.2/JA/grid/tooltips/fiddle/demo.js
@@ -5,11 +5,11 @@ $(function () {
 		renderCheckboxes: true,
         width: "100%",
         columns: [
-            { headerText: "$$ (AdventureWorks_Products_Name"), key: "Name", dataType: "string", width: "20%" },
-			{ headerText: "$$ (AdventureWorks_Products_ProductNumber"), key: "ProductNumber", dataType: "string", width: "20%" },
-			{ headerText: "$$ (AdventureWorks_Products_MakeFlag)", key: "MakeFlag", dataType: "bool", width: "10%" },
-			{ headerText: "$$ (AdventureWorks_Products_ReorderPoint)", key: "ReorderPoint", dataType: "number", width: "10%" },
-			{ headerText: "$$ (AdventureWorks_Products_SellStartDate)", key: "SellStartDate", dataType: "date", width: "15%" }
+            { headerText: "製品名", key: "Name", dataType: "string", width: "20%" },
+			{ headerText: "製品番号", key: "ProductNumber", dataType: "string", width: "20%" },
+			{ headerText: "メーカー フラグ", key: "MakeFlag", dataType: "bool", width: "10%" },
+			{ headerText: "再注文", key: "ReorderPoint", dataType: "number", width: "10%" },
+			{ headerText: "販売の開始日付", key: "SellStartDate", dataType: "date", width: "15%" }
         ],
         dataSource: adventureWorks.slice(0, 10),
         features: [
